09b3aafeac1fdd93c88be84f5cd6bb349b558d05
DerekMason-Gateshead <64601140+DerekMason-Gateshead@users.noreply.github.com>
2020-05-03 13:29:14 +0100

    Add files via upload

---
+++ Data/VeggieData.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Veggie_Recipes.Data
+{
+    public class VeggieData
+    {
+        public static List<string> listIngredients = new List<string>(); // { "cheese", "carrot" };
+        public static string query = "";
+    }
+}
